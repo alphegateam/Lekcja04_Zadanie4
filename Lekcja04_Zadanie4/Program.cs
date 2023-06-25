3890c5c7a521d85c7e18b48c40d7e426bb8c89b9
manx <manx@DESKTOP-95I9G38>
2023-06-25 22:07:35 +0200

    Add project files.

---
+++ Lekcja04_Zadanie4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace Zadanie_4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lekcja 4, zadanie dowowe 4.
+            // Napisz program w którym stworzysz 2 zmienne liczbowe, oraz jedną tekstową
+            string a;
+            int b;
+            float c;
+
+            a = "Szkoła Dotneta";
+            c = 12.5f;
+            b = 10;
+       
+        }
+    }
+}
+
+
